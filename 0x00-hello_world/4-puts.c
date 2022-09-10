bb635bb1a57e2effda30ba4e00e899ea2d77a73d
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-10 14:52:35 -0700

    using puts to print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- *
  * main - using the puts function
  *
  * Return: 0
- *
  */
 
 int main(void)
-
-{	
-	puts("\"Programming is like building a multilingual puzzle");			return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");			return (0);
 }
