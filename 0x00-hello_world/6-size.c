0c77b51258544a08e050b1304cdb0b366fa33474
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-12 03:58:42 -0700

    sizes of computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@
 int main(void)
 
 {
+char = 1, int = 4, long int = 4, long long int = 8, float = 4;
 printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
