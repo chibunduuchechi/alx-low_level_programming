0bb3a782d5e365488d5507c06dcb8afd4481a85e
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-14 11:06:21 -0700

    printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Description: print_alphabet
+ */
+
+void print_alphabet(void)
+
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; ++alphabet)
+	_putchar(alphabet);
+	_putchar('\n');
+}
