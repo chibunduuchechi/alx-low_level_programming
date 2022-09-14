6ad34c9a476be2dec2404732131031dbcd97842d
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-14 11:43:45 -0700

    print ten lines of alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - function printing the alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int line, alphabet;
+
+	for (line = 0; line <= 9; ++line)
+	{
+		for alphabet = 'a'; alphabet <= 'z'; ++alphanet)
+		_putchar(alphabet);
+		_putchar('\n');
+	}
+}
