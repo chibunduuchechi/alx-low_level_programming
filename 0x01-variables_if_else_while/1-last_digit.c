e8e054f5a613fdf96112c04b1e304b63303a8905
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-16 10:16:17 -0700

    printing the last digits of random numers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,15 +22,15 @@ int main(void)
 
 	if (last_dgt > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n", n, last_dgt);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_dgt);
 	}
 	else if (last_dgt == 0)
 	{
-		printf("last digit of %d is %d and is 0\n", n, last_dgt);
+		printf("Last digit of %d is %d and is 0\n", n, last_dgt);
 	}
 	else if (last_dgt < 6 && last_dgt != 0)
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, last_dgt);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_dgt);
 	}
 
 	return (0);
