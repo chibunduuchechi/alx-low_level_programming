07fdbba9eb91f994f32bcf81d328d52d4b2c5e7d
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-16 21:55:13 -0700

    a function checking uppercase characters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: function that checks for uppercase
+ *
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	int upper;
+
+	if (upper >= 65 && upper <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
