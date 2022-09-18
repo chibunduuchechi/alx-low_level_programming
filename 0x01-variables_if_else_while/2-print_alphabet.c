a9d9a9ca13e19577e96a8331bbf06e4a1aaca8b3
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-18 11:42:29 -0700

    printing the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: printing alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabets = 'a';
+
+	while (alphabets <= 'z')
+	{
+		putchar(alphabets);
+		++alphabets;
+	}
+	putchar('\n');
+
+	return (0);
+}
