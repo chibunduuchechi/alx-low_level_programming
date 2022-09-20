c9a5b6c672705864f3b96763c30a81801c9eba25
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-20 06:14:09 -0700

    printing all possible two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry point
+ *
+ * Description: printing two digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num1 = 0;
+
+	while (num1 <= 99)
+	{
+		int num2 = 0;
+
+		while (num2 <= 99)
+		{
+			if (num2 != num1)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+
+				if (num1 != 98 || num2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++num2;
+		}
+		++num1;
+	}
+	putchar('\n');
+	return (0);
+}
