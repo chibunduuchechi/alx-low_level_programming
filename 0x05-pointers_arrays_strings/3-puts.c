042dcab9c7133423e198fd408139be650d8494df
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-25 22:50:38 -0700

    printing a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - main entry point
+ *
+ * Description: prints a string to stdout
+ *
+ * @str: string peremeter to be printed
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
