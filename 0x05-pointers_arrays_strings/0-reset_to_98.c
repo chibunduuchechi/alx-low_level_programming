59b76900833464917e70634b012dced3a51bdeb6
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-25 18:11:02 -0700

    replacing n with 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updatimg the value of n to 98
+ * @n: integer pointer to a variable
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
