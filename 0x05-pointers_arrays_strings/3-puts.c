4810214fec52500d3a5459541cce566d0461948a
chibunduuchechi <chibunduuchechiahamefula.com>
2022-09-25 23:09:07 -0700

    printing string to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -12,10 +12,12 @@
 
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int c = 0;
+
+	while (str[c] != '\0')
 	{
-		_putchar(*str + 0);
-		++str;
+		_putchar(str[c]);
+		c++;
 	}
 	_putchar('\n');
 }
