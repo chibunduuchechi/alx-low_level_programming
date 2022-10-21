6a2b5b52324f10b0d381597b3403f987910cb468
chibunduuchechi <chibunduuchechiahamefula.com>
2022-10-21 09:59:42 -0700

    adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -28,9 +28,12 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	for (len = 0; str[len];)
 		len++;
+
 	new->str = dup;
 	new->len = len;
 	new->next = *head;
+
+	*head = new;
 
 	return (new);
 }
