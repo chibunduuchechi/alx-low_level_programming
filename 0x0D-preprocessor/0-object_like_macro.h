d1ebfc134e6ce85f032235938ec61febd5532865
chibunduuchechi <chibunduuchechiahamefula.com>
2022-10-21 23:59:32 -0700

    a header file that defines a macro named SIZE as an abbreviation for token 1024

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Description: a header file that describes a macro named size
+ * as abbreviation for the token 1024
+ */
+
+#define SIZE 1024
+
+#endif /*OBJECT_LIKE_MACRO_H*/
