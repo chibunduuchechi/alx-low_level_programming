aa7b5cb5bc61eea875796aa1d8e5894ba45575c3
chibunduuchechi <chibunduuchechiahamefula.com>
2022-10-27 16:33:13 -0700

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,28 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int m;
-	unsigned int val = 0;
+	unsigned int uint, val;
 
 	if (!b)
 		return (0);
 
-	for (m = 0; b[m] != '\0';  m++)
+	for (m = 0; b[m]; m++)
+		;
+	m--;
+	for (val = 1, uint = 0; m >= 0; m--)
 	{
-		val <<= 1;
-		if (b[m] == '1')
-			val += 1;
-		else
+		if (b[m] == '0')
 		{
-			if (b[m] != '0' && b[m] != '1')
-				return (0);
+			val *= 2;
+			continue;
 		}
+		else if (b[m] == '1')
+		{
+			uint += val;
+			val *= 2;
+			continue;
+		}
+		return (0);
 	}
-	return (val);
+	return (uint);
 }
